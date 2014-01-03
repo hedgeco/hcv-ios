28a3bc7c51a612fcaccb2a633a97716155f3b68d
David Cabrera <david@rerainc.com>
2014-01-03 09:58:47 -0500

    Initial Commit

---
+++ Vest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Vest
+//
+//  Created by David Cabrera on 1/3/14.
+//  Copyright (c) 2014 HedgeCo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Vest/SecondViewController.m
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.m
+//  Vest
+//
+//  Created by David Cabrera on 1/3/14.
+//  Copyright (c) 2014 HedgeCo. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
